8d2f3834515f34e982a90d9c6079c351df55412d
Vladimir <74735618+qsoulior@users.noreply.github.com>
2024-06-24 19:25:13 +0300

    Implement selection sort

---
+++ sort_test.go
@@ -0,0 +1,30 @@
+package alg
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSelectionSort(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"NilSlice", args{nil}, nil},
+		{"EmptySlice", args{[]int{}}, []int{}},
+		{"UnsortedSlice", args{[]int{6, 2, 3, 9, 1, 4, 1}}, []int{1, 1, 2, 3, 4, 6, 9}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.args.arr
+			SelectionSort(got, func(a int, b int) int { return a - b })
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SelectionSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
