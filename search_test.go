441fef9c894d9f37c88c274778cf8475649b99e7
Vladimir <74735618+qsoulior@users.noreply.github.com>
2024-06-24 17:37:16 +0300

    Implement binary search

---
+++ search_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestBinarySearch(t *testing.T) {
+	type args struct {
+		arr  []int
+		item int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"ExistingItem", args{[]int{1, 3, 5, 5, 7, 9}, 7}, 4},
+		{"NonExistentItem", args{[]int{1, 3, 5, 5, 7, 9}, 2}, -1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := BinarySearch(tt.args.arr, tt.args.item); got != tt.want {
+				t.Errorf("binarySearch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
