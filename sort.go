17369315112378b17a11056e15ecd319bf825cd4
Vladimir <74735618+qsoulior@users.noreply.github.com>
2024-06-25 23:09:43 +0300

    Implement quick sort

--- sort.go
+++ sort.go
@@ -1,5 +1,7 @@
 package alg
 
+import "math/rand"
+
 // Сортировка выбором, работающая за O(n^2)
 func SelectionSort[S ~[]E, E any](arr S, cmp func(E, E) int) {
 	n := len(arr) // длина массива
@@ -15,4 +17,35 @@ func SelectionSort[S ~[]E, E any](arr S, cmp func(E, E) int) {
 		// Меняем местами текущий элемент и наименьший элемент неотсортированной части.
 		arr[i], arr[min] = arr[min], arr[i]
 	}
+}
+
+// Рекурсивная быстрая сортировка, работающая за O(n*log(n))
+func QuickSort[S ~[]E, E any](arr S, cmp func(E, E) int) {
+	n := len(arr) // длина массива
+	if n < 2 {
+		// Если массив содержит 0 или 1 элемент, он отсортирован.
+		return
+	}
+
+	left, right := 0, n-1 // границы массива (индексы первого и последнего элементов)
+	pivot := rand.Intn(n) // индекс опорного элемента
+
+	// Меняем местами опорный и последний элементы.
+	arr[pivot], arr[right] = arr[right], arr[pivot]
+
+	for i := 0; i < right; i++ {
+		// Если элемент меньше опорного,
+		// отправляем его в начало после других отправленных в начало элементов.
+		if cmp(arr[i], arr[right]) < 0 {
+			arr[i], arr[left] = arr[left], arr[i]
+			left++
+		}
+	}
+
+	// Меняем местами опорный элемент и первый элемент,
+	// который больше или равен опорному.
+	arr[left], arr[right] = arr[right], arr[left]
+
+	QuickSort(arr[:left], cmp)
+	QuickSort(arr[left+1:], cmp)
 }
--- sort_test.go
+++ sort_test.go
@@ -28,3 +28,27 @@ func TestSelectionSort(t *testing.T) {
 		})
 	}
 }
+
+func TestQuickSort(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"NilSlice", args{nil}, nil},
+		{"EmptySlice", args{[]int{}}, []int{}},
+		{"UnsortedSlice", args{[]int{6, 2, 3, 9, 1, 4, 1}}, []int{1, 1, 2, 3, 4, 6, 9}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.args.arr
+			QuickSort(got, func(a int, b int) int { return a - b })
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("QuickSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
