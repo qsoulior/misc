dc3136a57e9b7750b98081030c367b8fd6550ecf
Vladimir <74735618+qsoulior@users.noreply.github.com>
2024-07-31 22:48:13 +0300

    Implement bubble sort and cocktail sort algorithms

--- slice/sort.go
+++ slice/sort.go
@@ -4,13 +4,66 @@ package slice
 
 import "math/rand"
 
+// BubbleSort sorts slice s in order as determined by cmp function.
+// It uses bubble sort algorithm with complexity O(n^2).
+// cmp should return 0 if a is equal b, a negative number if a precedes b,
+// or a positive number if a follows b.
+func BubbleSort[S ~[]E, E any](s S, cmp func(a E, b E) int) {
+	for i := len(s) - 1; i > 0; i-- {
+		swapped := false
+		// Move the largest element to the end of unsorted slice part.
+		for j := 0; j < i; j++ {
+			if cmp(s[j], s[j+1]) > 0 {
+				s[j], s[j+1] = s[j+1], s[j]
+				swapped = true
+			}
+		}
+		if !swapped {
+			break
+		}
+	}
+}
+
+// CocktailSort sorts slice s in order as determined by cmp function.
+// It uses cocktail sort (bidirectional bubble sort) algorithm with complexity O(n^2).
+// cmp should return 0 if a is equal b, a negative number if a precedes b,
+// or a positive number if a follows b.
+func CocktailSort[S ~[]E, E any](s S, cmp func(a E, b E) int) {
+	left, right := 0, len(s)-1 // boundaries of unsorted slice part
+	swapped := true            // if no elements have been swapped, then slice is sorted
+	for left < right && swapped {
+		// Move the largest element to the end of unsorted slice part.
+		swapped = false
+		for i := left; i < right; i++ {
+			if cmp(s[i], s[i+1]) > 0 {
+				s[i], s[i+1] = s[i+1], s[i]
+				swapped = true
+			}
+		}
+		if !swapped {
+			break
+		}
+		right-- // decrease right boundary of unsorted slice part
+
+		// Move the smallest element to the beginning of unsorted slice part.
+		swapped = false
+		for i := right; i > left; i-- {
+			if cmp(s[i], s[i-1]) < 0 {
+				s[i], s[i-1] = s[i-1], s[i]
+				swapped = true
+			}
+		}
+		left++ // increase left boundary of unsorted slice part
+	}
+}
+
 // SelectionSort sorts slice s in order as determined by cmp function.
 // It uses selection sort algorithm with complexity O(n^2).
 // cmp should return 0 if a is equal b, a negative number if a precedes b,
 // or a positive number if a follows b.
 func SelectionSort[S ~[]E, E any](s S, cmp func(a E, b E) int) {
 	n := len(s)
-	for i := 0; i < n; i++ {
+	for i := 0; i < n-1; i++ {
 		// Search for index of the smallest element in unsorted part of slice.
 		min := i
 		for j := i + 1; j < n; j++ {
@@ -20,7 +73,9 @@ func SelectionSort[S ~[]E, E any](s S, cmp func(a E, b E) int) {
 		}
 
 		// Swap current element and the smallest element of unsorted part.
-		s[i], s[min] = s[min], s[i]
+		if i != min {
+			s[i], s[min] = s[min], s[i]
+		}
 	}
 }
 
--- slice/sort_test.go
+++ slice/sort_test.go
@@ -5,6 +5,54 @@ import (
 	"testing"
 )
 
+func TestBubbleSort(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"NilSlice", args{nil}, nil},
+		{"EmptySlice", args{[]int{}}, []int{}},
+		{"UnsortedSlice", args{[]int{6, 2, 3, 9, 1, 4, 1}}, []int{1, 1, 2, 3, 4, 6, 9}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.args.arr
+			BubbleSort(got, func(a int, b int) int { return a - b })
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BubbleSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCocktailSort(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"NilSlice", args{nil}, nil},
+		{"EmptySlice", args{[]int{}}, []int{}},
+		{"UnsortedSlice", args{[]int{6, 2, 3, 9, 1, 4, 1}}, []int{1, 1, 2, 3, 4, 6, 9}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.args.arr
+			CocktailSort(got, func(a int, b int) int { return a - b })
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CocktailSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestSelectionSort(t *testing.T) {
 	type args struct {
 		arr []int
